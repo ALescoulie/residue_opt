9c797ca81395c95f34fb39aa19da2c7fcb8f6500
ALescoulie <alescoul@calpoly.edu>
2021-12-29 21:34:35 -0800

    Commit run_opts.py

---
+++ run_opts.py
@@ -0,0 +1,23 @@
+import MDAnalysis as mda
+
+import psi4
+
+from rdkit import Chem
+
+
+def get_spin_multiplicity(molecule: Chem.Mol) -> int:
+    """Returns the spin multiplicity of a :class:`RDKit.Mol`.
+    Based on method in http://www.mayachemtools.org/docs/modules/html/code/RDKitUtil.py.html .
+
+    :Arguments:
+        *molecule*
+            :class:`RDKit.Mol` object
+    """
+    radical_electrons: int = 0
+
+    for atom in molecule.GetAtoms():
+        radical_electrons += atom.GetNumRadicalElectrons()
+
+    total_spin: int = radical_electrons // 2
+    spin_mult: int = total_spin + 1
+    return spin_mult
